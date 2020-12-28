be8d5a6439a38f33545f9726c49787030e7190c7
Katharine Chandler <katharinechandler@katharines-mbp.home>
2020-12-28 13:04:16 -0500

    changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const API_KEY = "80oSbt3JJJUEWMEM8GBkw4G7sIl9h5ti"
 const BASE_URL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=music&api-key=${API_KEY}`
 const button = document.querySelector("button")
 const input = document.querySelector('input');
-const articleSection = document.querySelector('#article-list');
+const articleSection = document.querySelector('.responses');
 
 
 async function getData() {
@@ -31,9 +31,9 @@ function displayArticles(articles) {
 
 
 function removeArticlelist() {
-  const oldArticles = document.getElementById("article-list")
-  while (oldArticles.firstChild) {
-    oldArticles.removeChild(oldArticles.firstChild)
+  // const oldArticles = document.getElementById("article-list")
+  while (articleSection.firstChild) {
+    articleSection.removeChild(articleSection.firstChild)
   }
   // oldArticles.querySelectorAll("*").forEach(n=>n.remove())
 }
