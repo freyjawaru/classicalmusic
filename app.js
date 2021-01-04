afe2963fa6e6c5dee3028c8f99a8bc3b8f9df027
Katharine Chandler <katharinechandler@katharines-mbp.home>
2021-01-04 15:39:20 -0500

    monday afternoon

--- app.js
+++ app.js
@@ -6,35 +6,51 @@ const articleSection = document.querySelector('.responses');
 
 
 async function getData() {
-  let response = await axios.get(`${BASE_URL}&fq=${input.value}`)
-  removeArticlelist()
-  displayArticles(response.data.response.docs)
+  try {
+
+    const response = await axios.get(`${BASE_URL}&fq=${input.value}`)
+    removeArticlelist()
+    displayArticles(response.data.response.docs)
+
+    // request
+  } catch (err) {
+    console.log(err);
+    
+  }
 }
+ 
+
 
 
 button.addEventListener('click',getData)
 
-function displayArticles(articles) {
-  articles.forEach((article) => {
-    console.log(article)
-    let articleDiv = document.createElement("div")
-    articleDiv.innerHTML = `
-    <h2>${article.headline.main}</h2>
+  function displayArticles(articles) {
+    articles.forEach((article) => {
+      let articleDiv = document.createElement("div")
+      // if (article.multimedia[0].url!==undefined) {
+      articleDiv.innerHTML = `
+    <a href="${article.web_url}">${article.headline.main}</a>
     <p>${article.byline.original}</p>
     <img src="https://static01.nyt.com/${article.multimedia[0].url}"/>`
-    articleDiv.setAttribute (`id`, "articles")
+        console.log(article.multimedia[0].url)
+      articleDiv.setAttribute(`id`, "articles")
 
-    articleSection.append(articleDiv)
-    console.log(articleSection)
-  })
-}
+      articleSection.append(articleDiv)
+      //   }
+      // else {
+      //   console.log("frogs")
+
+      // }
+    
+      })
+    }
 
 
 function removeArticlelist() {
-  // const oldArticles = document.getElementById("article-list")
-  while (articleSection.firstChild) {
-    articleSection.removeChild(articleSection.firstChild)
-  }
-  // oldArticles.querySelectorAll("*").forEach(n=>n.remove())
-}
+        // const oldArticles = document.getElementById("article-list")
+        while (articleSection.firstChild) {
+          articleSection.removeChild(articleSection.firstChild)
+        }
+        // oldArticles.querySelectorAll("*").forEach(n=>n.remove())
+      }
   
