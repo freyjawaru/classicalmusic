9d8342021fcfe6361f2305bdd2bf7751ffe7d954
Katharine Chandler <katharinechandler@katharines-mbp.home>
2021-01-05 10:34:38 -0500

    color changes

--- app.js
+++ app.js
@@ -12,10 +12,7 @@ async function getData() {
     removeArticlelist()
     displayArticles(response.data.response.docs)
 
-    // request
   } catch (err) {
-    console.log(err);
-    
   }
 }
  
@@ -27,30 +24,22 @@ button.addEventListener('click',getData)
   function displayArticles(articles) {
     articles.forEach((article) => {
       let articleDiv = document.createElement("div")
-      // if (article.multimedia[0].url!==undefined) {
       articleDiv.innerHTML = `
     <a href="${article.web_url}">${article.headline.main}</a>
     <p>${article.byline.original}</p>
     <img src="https://static01.nyt.com/${article.multimedia[0].url}"/>`
-        console.log(article.multimedia[0].url)
       articleDiv.setAttribute(`id`, "articles")
 
       articleSection.append(articleDiv)
-      //   }
-      // else {
-      //   console.log("frogs")
-
-      // }
     
       })
     }
 
 
 function removeArticlelist() {
-        // const oldArticles = document.getElementById("article-list")
+ 
         while (articleSection.firstChild) {
           articleSection.removeChild(articleSection.firstChild)
         }
-        // oldArticles.querySelectorAll("*").forEach(n=>n.remove())
       }
   
